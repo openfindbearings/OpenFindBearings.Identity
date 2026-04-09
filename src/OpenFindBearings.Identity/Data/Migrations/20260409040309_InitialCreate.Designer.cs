b762bb33206287bb20282d481c2290568aca68e2
abcsxl <abcsxl@msn.com>
2026-04-09 12:04:11 +0800

    重建数据库迁移

--- src/OpenFindBearings.Identity/Data/Migrations/20260409040309_InitialCreate.Designer.cs
+++ src/OpenFindBearings.Identity/Data/Migrations/20260409040309_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using OpenFindBearings.Identity.Data;
 namespace OpenFindBearings.Identity.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260409030405_InitialCreate")]
+    [Migration("20260409040309_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
